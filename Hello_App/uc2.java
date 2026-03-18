f2d131ce914f1f00b4d3c3aea354f66be4ae58ba
Parthavi <parthavi05@gmail.com>
2026-03-18 14:50:12 +0530

    Updated

--- Hello_App/uc2.java
+++ Hello_App/uc2.java
@@ -1,12 +1,9 @@
 public class HelloApp {
     public static void main(String[] args) {
-
-        // Check if any command-line argument is provided
         if (args.length > 0) {
-            String name = args[0];  // First argument
+            String name = args[0]; 
             System.out.println("Hello, " + name + "!");
         } else {
-            // Default message if no argument is passed
             System.out.println("Hello, World!");
         }
     }
